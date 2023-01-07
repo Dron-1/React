d1f22146937db15bfebd6f95951a0de506f1c549
Dron-1 <67317674+Dron-1@users.noreply.github.com>
2023-01-07 22:21:22 +0530

    Create FeedbackData.js

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1, 
+        rating: 7,
+        feedbackText:"This is the first feedback. 7 is a nice feedback not gonna lie about it."
+    },
+    {
+        id: 2, 
+        rating: 8,
+        feedbackText:"This is the first feedback. 7 is a nice feedback not gonna lie about it."
+    },
+    {
+        id: 3, 
+        rating: 9,
+        feedbackText:"This is the first feedback. 7 is a nice feedback not gonna lie about it."
+    }
+]
+
+export default FeedbackData
