7815789bc70676306f9773f482dc0f757522c633
Dron-1 <67317674+Dron-1@users.noreply.github.com>
2023-01-20 20:47:25 +0530

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-//Entry point to react
-//import react from 'react'  // react library
-import reactDOM from 'react-dom' //used to interact with DOM,add our app to the browser
-import App from './App'
-import './index.css'
-
-/*reactDOM.render(component,where to put this component) */
-reactDOM.render(<App />,document.getElementById('root'))
